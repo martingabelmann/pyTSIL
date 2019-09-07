b253274650fa84f634f2e1681707db30b3139982
Martin Gabelmann <martin@gabelmann.biz>
2019-09-07 18:56:32 +0200

    add docs

--- README.md
+++ README.md
@@ -2,3 +2,18 @@
 
 (C)Python interface to [TSIL (Stephen P. Martin and David G. Robertson)](https://www.niu.edu/spmartin/TSIL/) [CPC 174
 (2006) 133-151](https://arxiv.org/abs/hep-ph/0501132)].
+
+## Install
+
+ * download and build [TSIL](https://www.niu.edu/spmartin/TSIL/)
+  * edit the Makefile to build with `-fPIC`
+ * build: `TSILDIR=/home/user/tsil python3 setup.py build && sudo -E python3 setup.py  install`
+   * `TSILDIR` should be the location where TSIL has been installed
+## Usage
+
+```
+from pyTSIL import TSIL
+results = TSIL(1,2,3,4,5,10,1) # yields all one- and two-loop functions computed by TSIL
+results["Mxyzuv"] # gives M(x=1,y=2,z=3,u=4,v=5,s=10,Q**2=1)
+```
+   
--- setup.py
+++ setup.py
@@ -1,4 +1,18 @@
 from distutils.core import setup, Extension
+from os import environ, path
+
+if 'TSILDIR' not in environ:
+    print('Environment variable $TSILDIR not set.\nPoint it to the location where TSIL has been built')
+    exit(2)
+
+if not path.isdir(environ['TSILDIR']):
+    print('$TSILDIR was not set to a valid dir.')
+    exit(2)
+
+if not path.isfile(path.join(environ['TSILDIR'],'libtsil.a')):
+    print('TSIL was not built.')
+    exit(2)
+
 setup(
         name = 'pyTSIL',
         version = '1.0',
@@ -7,8 +21,8 @@ setup(
                 'pyTSIL',
                 sources = ['pyTSIL.c'],
                 libraries = ['tsil'],
-                library_dirs = ['/home/martin/FeynTools/TSIL'],
-                include_dirs = ['/home/martin/FeynTools/TSIL']
+                library_dirs = [environ['TSILDIR']],
+                include_dirs = [environ['TSILDIR']]
                 )
             ]
         )
