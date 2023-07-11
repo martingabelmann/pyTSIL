9644e349d0e3ed2602c7f5909b4d1effd43d8885
Martin Gabelmann <martin@gabelmann.biz>
2023-07-11 11:52:31 +0200

    clear_cache + ability to return only real parts

--- pyTSIL/loopfunctions.py
+++ pyTSIL/loopfunctions.py
@@ -2,13 +2,41 @@ from .TSIL import TSIL
 from functools import lru_cache
 from cmath import log
 
-global q, qq
+global q, qq, REAL
+
+REAL = lambda x: x # noqa: E731
+
+def return_real(val=True):
+    """ val=True: all loop functions return only their real part.
+        val=False: all loop function return complex numbers.
+        By default all loop functions return complex numbers.
+    """
+    global REAL
+    if val:
+        REAL = lambda x: x.real # noqa: E731
+    else:
+        REAL = lambda x: x # noqa: E731
+    clear_cache()
+
+def clear_cache():
+    """ clears cache of all wrapper functions """
+    for func in [
+            A, B, dBds,dBdx, C,
+            S, I, T, TBAR, U, V, M,
+            Aeps, Beps, Ceps, dBdxeps,
+            S0, I0, T0, U0, V0, TSILc
+            ]:
+        info = func.cache_info()
+        if info.currsize > 0:
+            print(f'clearing cache of {func.__name__} ({info})')
+            func.cache_clear()
 
 def set_renscale(qren):
     """ sets the renormalization scale for all loop function wrappers (not the squared one!) """
     global q, qq
     q = qren
     qq = qren**2
+    clear_cache()
 
 @lru_cache(maxsize=None)
 def TSILc(x,y,z,u,v,s):
@@ -30,86 +58,86 @@ def A(x):
 @lru_cache(maxsize=None)
 def B(x,y,s):
     """ one-loop two-point function """
-    return TSILc(x,x,y,x,x,s)['Bxz']
+    return REAL(TSILc(x,x,y,x,x,s)['Bxz'])
 
 @lru_cache(maxsize=None)
 def dBds(x,y,s):
     """ derivative of one-loop two-point function w.r.t. squared external momentum """
-    return TSILc(x,x,y,x,x,s)['dBdsxz']
+    return REAL(TSILc(x,x,y,x,x,s)['dBdsxz'])
 
 @lru_cache(maxsize=None)
 def dBdx(x,y,s):
     """ derivative of one-loop two-point function w.r.t. first squared mass argument"""
-    return TSILc(x,x,y,x,x,s)['Bpxz']
+    return REAL(TSILc(x,x,y,x,x,s)['Bpxz'])
 
 @lru_cache(maxsize=None)
 def C(x,y,z,s):
     """ one-loop two-point function  (C0 integral)"""
-    return TSILc(x,y,z,x,x,s)['Cfinxyz']
+    return REAL(TSILc(x,y,z,x,x,s)['Cfinxyz'])
 # two-loop
 @lru_cache(maxsize=None)
 def S(x,y,z,s):
-    return TSILc(x,x,x,y,z,s)['Sxuv']
+    return REAL(TSILc(x,x,x,y,z,s)['Sxuv'])
 
 @lru_cache(maxsize=None)
 def I(x,y,z): # noqa: E741, E743
-    return S(x,y,z,0)
+    return REAL(S(x,y,z,0))
 
 @lru_cache(maxsize=None)
 def T(x,y,z,s):
-    return TSILc(x,x,x,y,z,s)['Txuv']
+    return REAL(TSILc(x,x,x,y,z,s)['Txuv'])
 
 @lru_cache(maxsize=None)
 def TBAR(x,y,z,s):
-    return TSILc(x,x,x,y,z,s)['TBARxuv']
+    return REAL(TSILc(x,x,x,y,z,s)['TBARxuv'])
 
 @lru_cache(maxsize=None)
 def U(x,y,z,u,s):
-    return TSILc(x,y,y,z,u,s)['Uxzuv']
+    return REAL(TSILc(x,y,y,z,u,s)['Uxzuv'])
 
 @lru_cache(maxsize=None)
 def V(x,y,z,u,s):
-    return TSILc(x,y,y,z,u,s)['Vxzuv']
+    return REAL(TSILc(x,y,y,z,u,s)['Vxzuv'])
 
 @lru_cache(maxsize=None)
 def M(x,y,z,u,v,s):
-    return TSILc(x,y,z,u,v,s)['Mxyzuv']
+    return REAL(TSILc(x,y,z,u,v,s)['Mxyzuv'])
 
 # O(eps) pieces of one-loop functions
 @lru_cache(maxsize=None)
 def Aeps(x):
-    return TSILc(x,x,x,x,x,0)['Aepsx']
+    return REAL(TSILc(x,x,x,x,x,0)['Aepsx'])
 
 @lru_cache(maxsize=None)
 def Beps(x,y,s):
-    return TSILc(x,x,y,x,x,s)['Bepsxz']
+    return REAL(TSILc(x,x,y,x,x,s)['Bepsxz'])
 
 @lru_cache(maxsize=None)
 def Ceps(x,y,z,s):
-    return TSILc(x,y,z,x,x,s)['Cepsxyz']
+    return REAL(TSILc(x,y,z,x,x,s)['Cepsxyz'])
 
 @lru_cache(maxsize=None)
 def dBdxeps(x,y,s):
-    return TSILc(x,x,y,x,x,s)['Bepsprimexz']
+    return REAL(TSILc(x,x,y,x,x,s)['Bepsprimexz'])
 
 
 # two-loop functions w/ eps^0 pieces
 @lru_cache(maxsize=None)
 def S0(x,y,z,s):
-    return TSILc(y,x,x,x,z,s)['SSuxv0']
+    return REAL(TSILc(y,x,x,x,z,s)['SSuxv0'])
 
 @lru_cache(maxsize=None)
 def I0(x,y,z):
-    return S0(x,y,z,0)
+    return REAL(S0(x,y,z,0))
 
 @lru_cache(maxsize=None)
 def T0(x,y,z,s):
-    return TSILc(x,x,x,y,z,s)['TTxuv0']
+    return REAL(TSILc(x,x,x,y,z,s)['TTxuv0'])
 
 @lru_cache(maxsize=None)
 def U0(x,y,z,u,s):
-    return TSILc(x,y,y,z,u,s)['UUxzuv0']
+    return REAL(TSILc(x,y,y,z,u,s)['UUxzuv0'])
 
 @lru_cache(maxsize=None)
 def V0(x,y,z,u,s):
-    return TSILc(x,y,y,z,u,s)['VVxzuv0']
+    return REAL(TSILc(x,y,y,z,u,s)['VVxzuv0'])
